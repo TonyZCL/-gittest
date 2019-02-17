2aefff68d3821f526daf3215735cd1fb1ba27b3f
conglang <1425869196@qq.com>
2019-02-17 23:05:18 +0800

    门户系统首页搜索跳转

---
+++ pinyougou/pinyougou-search/pinyougou-search-interface/src/main/java/com/pinyougou/search/service/ItemSearchService.java
@@ -0,0 +1,15 @@
+package com.pinyougou.search.service;
+
+import java.util.Map;
+
+public interface ItemSearchService {
+
+    /**
+     * 根据搜索关键字搜索商品列表
+     * @param searchMap
+     * @return 搜索结果
+     */
+    Map<String,Object> search(Map<String, Object> searchMap);
+
+
+}
